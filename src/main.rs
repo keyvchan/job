c9714e5e5ce0ac60955120c8c64e70067cae581d
Keyv Chan <keyvchan@gmail.com>
2022-12-05 21:24:27 +0800

    feat: list processes

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+use libproc::libproc::{
+    bsd_info::BSDInfo,
+    proc_pid::{listpids, name, pidinfo, ProcType},
+};
+
 fn main() {
-    println!(r#"hello"#)
+    if let Ok(pids) = listpids(ProcType::ProcAllPIDS) {
+        println!("Found {} processes using listpids()", pids.len());
+        for pid in pids {
+            println!(
+                "Process: pid {}, {}",
+                pid,
+                name(pid as i32).unwrap_or_else(|_| "Unknown".to_string())
+            );
+        }
+    }
 }
